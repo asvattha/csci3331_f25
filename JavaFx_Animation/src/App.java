beb412cd4e9b72243d39ab2b920e6ddcbfe8bd0c
asvattha <singhd1@sfasu.edu>
2025-10-21 12:43:58 -0500

    listeners for observable objects

--- JavaFx_Animation/src/App.java
+++ JavaFx_Animation/src/App.java
@@ -1,10 +1,12 @@
 import javafx.application.Application;
+import javafx.beans.Observable;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.beans.InvalidationListener;
 
 public class App extends Application{
     
@@ -30,13 +32,32 @@ public class App extends Application{
          * You can bind a target object (of Property type) to a source object. A change in the source object will be automatically
          * reflected in the target object.
         */
-
+/* 
         DoubleProperty x = new SimpleDoubleProperty(5.5);
         DoubleProperty y = new SimpleDoubleProperty(7.5);
         System.out.println("x : " + x.getValue() + "y : " + y.getValue());
         x.bind(y);
         System.out.println("x : " + x.getValue() + "y : " + y.getValue());
+*/
 
+        // Listeners for Observable Objects
+        /*
+         * For more details, please read the section 15.10 from the Introduction to Java programming by Y D Liang
+         * You can add a listener to process a value change in an observable object. 
+         * An instance of Observable is known as an observable object, which contains the addListener(InvalidationListener listener) method for adding a listener. The listener class must implement the functional interface InvalidationListener to overrride the invalidated(Observable ov) method for handling the value change. Once the value is changed in the Observable object, the listener is notified by invoking its invalidated(Observable ov) method. Evenr binding property is an instance of Observable such as DoubleProperty, IntegerProperty, BooleanProperty etc. 
+         */
+
+         DoubleProperty balance = new SimpleDoubleProperty();
+         balance.addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable ov) {
+                System.out.println("The new value is " + balance.getValue());
+                System.out.println("Take more actions once the balance has changed");
+            }
+         });
+
+         balance.setValue(4.5);
+         
         Application.launch(args);
     }
 }
